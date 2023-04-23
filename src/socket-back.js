2e01348f1bc90be658682b7af71b7d5d3dd31dd7
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2023-04-23 03:02:06 -0300

    04

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,48 +1,24 @@
+import { atualizaDocumento, encontrarDocumento } from "./documentosDb.js";
 import io from "./servidor.js";
-
-const documentos = [
-  {
-    nome: "JavaScript",
-    texto: "texto de javascript...",
-  },
-  {
-    nome: "Node",
-    texto: "texto de node...",
-  },
-  {
-    nome: "Socket.io",
-    texto: "texto de socket.io...",
-  },
-];
 
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou! ID:", socket.id);
 
-  socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
+  socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
     socket.join(nomeDocumento);
 
-    const documento = encontrarDocumento(nomeDocumento);
+    const documento = await encontrarDocumento(nomeDocumento);
 
     if (documento) {
       devolverTexto(documento.texto);
     }
   });
 
-  socket.on("texto_editor", ({ texto, nomeDocumento }) => {
-    const documento = encontrarDocumento(nomeDocumento);
+  socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
+    const atualizacao = await atualizaDocumento(nomeDocumento, texto);
 
-    if (documento) {
-      documento.texto = texto;
-
+    if (atualizacao.modifiedCount) {
       socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
     }
   });
 });
-
-function encontrarDocumento(nome) {
-  const documento = documentos.find((documento) => {
-    return documento.nome === nome;
-  });
-
-  return documento;
-}
