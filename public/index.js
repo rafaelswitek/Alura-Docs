b6f0155c7dae4f137af6ebed5ea41a5172c18da3
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2023-04-23 20:01:37 -0300

    06

--- public/index.js
+++ public/index.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", (evento) => {
 function inserirLinkDocumento(nomeDocumento) {
   listaDocumentos.innerHTML += `
     <a
-      href="documento.html?nome=${nomeDocumento}"
+      href="/documento/index.html?nome=${nomeDocumento}"
       class="list-group-item list-group-item-action"
       id="documento-${nomeDocumento}"
     >
